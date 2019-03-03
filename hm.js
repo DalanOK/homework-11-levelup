7e29d07f3fbece8c0ae69d604916428c28260b9d
Maxim <Dalannok@gmail.com>
2019-03-03 16:15:46 +0200

    Create hm.js

---
+++ hm.js
@@ -0,0 +1,113 @@
+/* level 1 */
+for (let i = 0; i < 2; i++){
+   north()
+}
+
+/*level 2*/
+for(let i = 0; i <16;i++){
+ east();   
+}
+/* level 3*/
+for(let i = 0; i<7;i++){
+ south();   
+ east();   
+}
+/* level 4*/
+ for(let i = 0; i<1;i++){
+    south();
+}for(let i = 0; i<3;i++){
+    east();
+}for(let i = 0; i<2;i++){
+    south();
+}for(let i = 0; i<1;i++){
+    east();
+}for(let i = 0; i<3;i++){
+    south();
+}for(let i = 0; i<2;i++){
+    east();
+}for(let i = 0; i<1;i++){
+    south();
+}
+/* level 5 */
+  for(let i = 0;i<6;i++){
+    east();   
+   }  
+   for(let i = 0;i<2;i++){
+    south();   
+   }  
+   for(let i = 0;i<2;i++){
+    west();   
+   }  for(let i = 0;i<3;i++){
+    south();   
+   } 
+   for(let i = 0;i<2;i++){
+    west();   
+   } for(let i = 0;i<3;i++){
+    north();   
+   } 
+   for(let i = 0;i<2;i++){
+    west();   
+   } 
+   for(let i = 0;i<5;i++){
+    south();   
+   } 
+   for(let i = 0;i<6;i++){
+    east();   
+   } 
+
+ /* level 6 */
+  for(let i = 0;i<5;i++){
+    south();   
+   } for(let i = 0;i<2;i++){
+    east();   
+   } 
+   for(let i = 0;i<3;i++){
+    north();   
+   } for(let i = 0;i<1;i++){
+    east();   
+   } 
+   for(let i = 0;i<3;i++){
+    north();   
+   }
+   for(let i = 0;i<3;i++){
+    east();   
+   }
+   for(let i = 0;i<4;i++){
+    south();   
+   }
+   for(let i = 0;i<2;i++){
+    west();   
+   }
+   for(let i = 0;i<3;i++){
+    south();   
+   }
+   for(let i = 0;i<2;i++){
+    east();   
+   }
+   /* level 7 */
+   for(let i = 0;i<11;i++){
+    south();   
+   }for(let i = 0;i<4;i++){
+    east();   
+   }for(let i = 0;i<11;i++){
+    north();   
+   }for(let i = 0;i<2;i++){
+    east();
+    }for(let i = 0;i<1;i++){
+    south();
+    }for(let i = 0;i<1;i++){
+    east();
+    }for(let i = 0;i<5;i++){
+    south();
+    }for(let i = 0;i<1;i++){
+    west();
+    }for(let i = 0;i<7;i++){
+    south();
+    }for(let i = 0;i<8;i++){
+    east();
+    }for(let i = 0;i<9;i++){
+    south();
+    }for(let i = 0;i<1;i++){
+    east();
+    }
+   
